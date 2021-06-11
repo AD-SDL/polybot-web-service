ec63db334ef85e743a69af5a6665b31deaed081e
Logan Ward <ward.logan.t@gmail.com>
2021-06-11 10:29:21 -0400

    Refactored tests to make fake robot re-usable

--- polybot/tests/conftest.py
+++ polybot/tests/conftest.py
@@ -1,10 +1,24 @@
 from pathlib import Path
+from subprocess import Popen
+from time import sleep
 
 from pytest import fixture
 
 from polybot.models import Sample
+from polybot.config import settings
 
 _test_sample = str(Path(__file__).parent / 'files' / 'example-sample.json')
+_test_dir = Path(__file__).parent
+
+
+@fixture()
+def fake_robot():
+    settings.robot_url = "http://localhost:8152/"
+    proc = Popen(['uvicorn', '--port', '8152', '--app-dir', _test_dir, 'fake_robot:app'])
+    sleep(1)
+    assert proc.poll() is None, "Uvicorn process died"
+    yield proc
+    proc.kill()
 
 
 @fixture()
--- polybot/tests/test_robot.py
+++ polybot/tests/test_robot.py
@@ -1,24 +1,5 @@
-from time import sleep
-from pathlib import Path
-from subprocess import Popen
-
-from pytest import fixture
-
-from polybot.config import settings
 from polybot.robot import send_new_sample
 
-_test_dir = Path(__file__).parent
 
-
-@fixture(autouse=True)
-def fake_robot():
-    settings.robot_url = "http://localhost:8152/"
-    proc = Popen(['uvicorn', '--port', '8152', '--app-dir', _test_dir, 'fake_robot:app'])
-    sleep(1)
-    assert proc.poll() is None, "Uvicorn process died"
-    yield proc
-    proc.kill()
-
-
-def test_submit(example_sample):
+def test_submit(example_sample, fake_robot):
     send_new_sample(example_sample)
