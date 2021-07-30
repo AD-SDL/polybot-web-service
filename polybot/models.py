55b0c0d4e88234aaf35b9319db25f8bc45abf0ea
Logan Ward <ward.logan.t@gmail.com>
2021-07-30 11:50:00 -0400

    Allow for inclusive/exclusive ranges, use new format

--- polybot/models.py
+++ polybot/models.py
@@ -1,5 +1,5 @@
 """Data models for objects used by this service"""
-from typing import List, Dict, Any, Optional, Tuple, Iterable
+from typing import List, Dict, Any, Optional, Tuple, Iterable, Union
 from itertools import product
 from uuid import uuid4
 
@@ -47,12 +47,20 @@ class SampleTemplate(Sample):
 
     ID: Optional[str] = None
 
-    _inputs_info: Dict[str, str] = Field(default_factory=dict, help='Human-readable descriptions of each parameter')
+    inputs_info: Dict[str, str] = Field(default_factory=dict, help='Human-readable descriptions of each parameter')
 
     inputs_space: Dict[str, Optional[Tuple[float, float, float]]] = Field(
         default_factory=dict, help='Allowed ranges and values of the different input parameters. '
                                    'Tuples specify the minimum, maximum, and step size. '
                                    'Null values indicate parameters that must remain their default values'
+    )
+    inputs_interval: Dict[str, str] = Field(
+        default_factory=dict, help='Defines whether the range in inputs_space is inclusive or not, '
+                                   'or whether the value should be held constant',
+        regex=r'^(\[constant\]|[\(\[][0-9.]+,\s*[0-9.][0-9.]+[\)\]])$'
+    )
+    inputs_dtype: Dict[str, str] = Field(
+        default_factory=dict, help='The numerical type of the value of a field'
     )
 
     @property
@@ -86,6 +94,38 @@ class SampleTemplate(Sample):
         for vals in product(*acceptable_values):
             yield dict(zip(keys, vals))
 
+    def get_acceptable_values_for_field(self, field: str) -> List[Union[int, float]]:
+        """Get a list of acceptable values for a specific field
+
+        Args:
+            field: Name of the field
+        Returns:
+            List of acceptable values
+        """
+
+        assert field in self.inputs_interval, f"Field \"{field}\" does not have a defined interval"
+        assert field in self.inputs_dtype, f"Field \"{field}\" does not have a defined units"
+
+        if self.inputs_space[field] is None:
+            assert self.inputs_interval[field] == "[constant]",\
+                "Inconsistent template: Space is null and interval is not [constant]"
+            return [self.inputs[field]]
+        else:
+            assert self.inputs_interval[field] != "[constant]", \
+                "Inconsistent template: Space is not null and interval is [constant]"
+
+            # TODO: We ignore the values in "interval" and assume they are consistent with input_space
+            _min, _max, _step = self.inputs_space[field]
+            vals = np.arange(_min, _max + _step, _step,  # _max + _step makes the range inclusive
+                             dtype=self.inputs_dtype[field])
+
+            # Prune off the first and last entry if they are exclusive
+            if self.inputs_interval[field].startswith("("):
+                vals = vals[1:]
+            if self.inputs_interval[field].endswith(")"):
+                vals = vals[:-1]
+            return vals
+
     def list_acceptable_input_values(self) -> Dict[str, List[float]]:
         """Get the possible options for each input
 
@@ -93,21 +133,9 @@ class SampleTemplate(Sample):
             Dictionary where keys are the name of an input field and values are
             a list of acceptable values for that field
         """
-        # Loop over the input space and get the possible values for each function
-        acceptable_values = []
-        keys = []
-        for key, vals in self.inputs_space.items():
-            keys.append(key)  # Store the name of the input
-
-            # Store the acceptable values
-            if vals is None:
-                acceptable_values.append([self.inputs[key]])  # Store the default value
-            else:
-                _min, _max, _step = vals
-                acceptable_values.append(
-                    np.arange(_min, _max + _step, _step)  # _max + _step makes the range inclusive of _min and _max
-                )
-        return dict(zip(keys, acceptable_values))
+        # Loop over the input space and get the possible values for each input
+        return dict((key, self.get_acceptable_values_for_field(key))
+                    for key in self.inputs_space)
 
     def generate_search_space_dataframe(self) -> pd.DataFrame:
         """Create the search space as a Pandas DataFrame"""
--- polybot/tests/test_model.py
+++ polybot/tests/test_model.py
@@ -5,8 +5,9 @@ def test_generate_search_space(example_template):
     data = example_template.generate_search_space_dataframe()
     assert len(data.columns) == 15
     assert len(data["annealing_post.T"].value_counts()) == 1
-    assert len(data["prepare_solution.V_sol[1]"].value_counts()) == 6
-    assert len(data["coating_on_top.vel"].value_counts()) == 10
+    assert len(data["prepare_solution.V_sol[1]"].value_counts()) == 5  # Exclude top
+    assert len(data["coating_on_top.vel"].value_counts()) == 8  # Exclude both
+    assert len(data["post_processing.vel"].value_counts()) == 4  # Inclusive
 
 
 def test_generate_sample(example_template):
