77c192f609d56f6041d80701345248597cbaae90
Logan Ward <ward.logan.t@gmail.com>
2021-08-31 21:09:30 -0400

    Add a subscription iterator

--- polybot/sample.py
+++ polybot/sample.py
@@ -15,6 +15,29 @@ from .models import Sample
 logger = logging.getLogger(__name__)
 
 
+def subscribe_to_study() -> Iterator[Sample]:
+    """Subscribe to the "new sample" created event feed
+
+    Yields:
+        Latest samples as they are created
+    """
+
+    # Query to get the list of samples in the study
+    adc_client = settings.generate_adc_client()
+    if settings.adc_study_id is None:
+        raise ValueError('The ADC study id is not set. Set your ADC_STUDY_ID environment variable.')
+
+    for event in adc_client.subscribe_to_study(settings.adc_study_id):
+        # Check that we have the right type of event
+        if "newSample" not in event:
+            logger.debug('Event type was not "newSample"')
+            continue
+
+        # Get the sample information
+        sample = event["newSample"]["sample"]
+        yield _parse_sample(sample)
+
+
 def load_samples() -> Iterator[Sample]:
     """Load all of the known samples from disk
 
@@ -29,12 +52,20 @@ def load_samples() -> Iterator[Sample]:
     study_info = adc_client.get_study(settings.adc_study_id)
 
     for sample in study_info['study']['samples']:
-        # Pull down the JSON associated with each sample
-        json_url = sample['url']
-        sample_data = get(json_url, verify=False).json()
+        yield _parse_sample(sample)
 
-        # Yield the result a Sample object
-        try:
-            yield Sample.parse_obj(sample_data)
-        except Exception:
-            logger.warning(f'Failed to parse Sample ID {sample["id"]}')
+
+def _parse_sample(sample_record: dict) -> Sample:
+    """Create a Sample object given a sample record from ADC
+
+    Args:
+        sample_record: Sample information record from ADC
+    Returns:
+        Sample object in the format used by `pol
+    """
+    # Pull down the JSON associated with each sample
+    json_url = sample_record['url']
+    sample_data = get(json_url, verify=False).json()
+
+    # Parse as a sample object
+    return Sample.parse_obj(sample_data)
--- polybot/tests/test_samples.py
+++ polybot/tests/test_samples.py
@@ -1,29 +1,40 @@
-from polybot.sample import load_samples
+from pathlib import Path
+import json
 
-example_output = {
-    "study": {
-        "id": "U3R1ZHlOb2RlOjU=",
-        "name": "polybot-ai-test",
-        "description": "Test study for developing the Polybot AI planning service.",
-        "keywords": [],
-        "startDate": None,
-        "status": "NEW",
-        "created": "2021-08-30T16:54:41.706854+00:00",
-        "updated": "2021-08-30T16:54:42.127315+00:00",
-        "permissions": [
-            {
-                "user": {
-                    "id": "VXNlck5vZGU6NQ==",
-                    "name": "",
-                    "email": "loganw@uchicago.edu"
-                },
-                "level": "ADMIN"
-            }
-        ],
-        "investigations": [],
-        "samples": []
-    }
-}
+from pytest import fixture
+from pytest_mock import MockerFixture
+
+from polybot.config import settings
+from polybot.models import Sample
+from polybot.sample import load_samples, subscribe_to_study
+
+_my_path = Path(__file__).parent
+
+
+@fixture
+def example_sample() -> dict:
+    """Get an example sample response from ADC with a fresh token to access S3"""
+    client = settings.generate_adc_client()
+    assert settings.adc_study_id is not None, "Missing ADC_STUDY_ID environmental variable"
+    study_response = client.get_study(settings.adc_study_id)
+    return study_response['study']['samples'][0]
+
+
+@fixture()
+def mock_subscribe(example_sample, mocker: MockerFixture):
+    # Make a fake subscription response
+    ex_record = json.loads(_my_path.joinpath('files', 'example-adc-subscription-event.json').read_text())
+    ex_record['newSample']['sample'] = example_sample  # Gives a sample with an active data URL
+
+    # Mock the event generator
+    def _fake_subscribe(*args, **kwargs):
+        yield ex_record
+    mocker.patch('polybot.config.ADCClient.subscribe_to_study', new=_fake_subscribe)
+
+
+def test_subscribe(mock_subscribe):
+    sample = next(subscribe_to_study())
+    assert isinstance(sample, Sample)
 
 
 def test_load(example_sample):
